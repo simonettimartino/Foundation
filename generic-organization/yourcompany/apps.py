9a090a7dbac6a6c6b9ceedb3687ef27a992e2aad
Michele <bonini.michele15@icloud.com>
2021-03-27 13:21:51 +0100

    small fix | reentrant populate problema

--- generic-organization/yourcompany/apps.py
+++ generic-organization/yourcompany/apps.py
@@ -4,17 +4,16 @@ from antidote import world
 
 from generic_organization_service.handlers.organization_handler_manager import OrganizationHandlerManager
 from generic_organization.settings import LOGGING
-
+from yourcompany.organization_handler import OrganizationHandler
 
 class MyAppConfig(AppConfig):
     name = "yourcompany"
 
     def ready(self):
         # FIXME change the import with your organization folder
-        from organization_template.organization_handler import OrganizationHandler
+       
         handler_manager = world.get(OrganizationHandlerManager)
         handler_manager.add_organization_handler("yourcompany", OrganizationHandler())
-
         logger_conf = {
             'handlers': ['console'],
             'level': os.getenv('SEVERITY_LOG_LEVEL', 'INFO'),
--- generic-organization/yourcompany/organization_handler.py
+++ generic-organization/yourcompany/organization_handler.py
@@ -8,4 +8,22 @@ logger = logging.getLogger(__name__)
 
 
 class OrganizationHandler(OrganizationAbstractHandler):
-    pass
+    def handle_confirm_verify(self, request_uid: str, connection_id: str, presentation_id, request_data: dict()):
+        pass
+
+    def handle_connection_notify(self, request_uid: str, connection_id: str, request_data: dict()):
+        pass
+
+    def handle_confirm_issue(self, request_uid: str, connection_id: str, request_data: dict()):
+        pass
+    def handle_discard_credential(self, request_uid: str, connection_id: str, request_data: dict()):
+        pass
+
+    def handle_discard_proof(self, request_uid: str, connection_id: str, request_data: dict()):
+        pass
+
+    def handle_values_for_credential(self, request_uid: str, connection_id: str, request_data: dict()) -> list:
+        pass
+    
+    def handle_async_event(self, event_type: str, request_data: dict):
+        pass
